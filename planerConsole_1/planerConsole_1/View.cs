55ecd0b198fda6eb33783b7065fe5c7ea5ec4481
ramzes <adrian.ramzes@gmail.com>
2013-05-31 14:26:15 +0200

    zmiany w view

--- planerConsole_1/planerConsole_1/View.cs
+++ planerConsole_1/planerConsole_1/View.cs
@@ -6,31 +6,61 @@ namespace planerConsole_1
 {
 	public class View
 	{
+		public Controller C;
+
 		public View ()
 		{
-	
-		}
-		static public void WriteMessage(string txt)
-		{
-			Console.WriteLine(txt);
+			Console.WriteLine ("Czy chcesz utworyć nowy projekt? Y/N");
+			string ans = Console.ReadLine ();
+
+			if (ans == "y" || ans == "Y") 
+			{
+				Console.WriteLine ("Podaj Nazwę(sciezke) pliku:");
+				string filePath = Console.ReadLine ();
+
+				StreamWriter sw = new StreamWriter (filePath, false);
+				sw.Close ();
+
+				C = new Controller(filePath);
+			}
+			else
+			{
+				string filePath;
+				while(true)
+				{
+					Console.WriteLine("Podaj Sciezke do istniejącego pliku:");
+					filePath = Console.ReadLine(); 
+
+					if(File.Exists(filePath)) break;
+					else if(filePath == "exit") throw new Exception("exit");
+					else Console.WriteLine("Nie Poprawna sciezka!!");
+				}
+
+				C = new Controller(filePath);
+				Console.Write(">>:");
+			}
 		}
 
-		static public void DisplaySubNodesList (List<Node> subNodes)
+		public void Input (string line)
 		{
-			foreach (Node temp in subNodes) 
+			switch (line) 
 			{
-				Console.Write("{0}({1})\t",temp.name, temp.state);
+			case "ls":
+				Ls();
+				break;
+			case "help":
+				Help();
+				break;
+			case "progress":
+				Progress();
+				break;
+			case "tree":
+				// wywolanie tree
+				break;
+			default:
+				C.controllerInput(line);
+				break;
 			}
-			Console.WriteLine();
-		}
-
-		static public void DisplayPrevSubNodesList(List<Node> prevNodes)
-		{
-			foreach (Node temp in prevNodes) 
-			{
-				Console.Write("{0}({1})\t",temp.name, temp.state);
-			}
-			Console.WriteLine();
 		}
 
 	}
