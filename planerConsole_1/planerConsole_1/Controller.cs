a3d7bdf5043bb0ee456574971c6b156b7c80aa10
Kamil <kamil@kamil-AOD257.(none)>
2013-05-31 12:32:25 +0200

    zmiana na switch

--- planerConsole_1/planerConsole_1/Controller.cs
+++ planerConsole_1/planerConsole_1/Controller.cs
@@ -5,7 +5,6 @@ namespace planerConsole_1
 {
 	public class Controller
 	{
-		//public List<Command> commandList;
 		public Model mod;
 		public List<Node> subNodesList;
 		public List<Node> prevSubNodesList;
@@ -14,46 +13,152 @@ namespace planerConsole_1
 
 		public Controller (Model mod)
 		{
+			this.mod = mod;
 			subNodesList = mod.GetSubNodesList(0);
 			prevSubNodesList = mod.GetPrevNodesList(0);
 			currentNode = null;
 			currentLvl = 0;
 
-//			commandList = new List<Command>();
-			this.mod = mod;
-//			initCommandlist();
 		}
-/*
-		public void initCommandlist()
-		{
-			commandList.Add (new CommandLS(this));
-			commandList.Add(new CommandCD(mod, this));
-			commandList.Add(new CommandCN(mod,this));
-			commandList.Add(new CommandCS(mod,this));
-			commandList.Add (new CommandADD(mod, this));
-			commandList.Add (new CommandDEL(mod,this));
-			commandList.Add(new CommandPRGS(mod,this));
-		
-			//pozostale komendy
-			commandList.Add (new CommandHELP(this));
-		}
-*/
+
 		public void controllerInput (string input)
 		{
 			string[] inputTable = input.Split (' ');
-			//przeszukiwanie listy komend i potem wykonywanie jej funkcji realizeCommand
-			foreach (Command lookingFor in commandList) {
-				if (lookingFor.commandName == inputTable [0]) {
-					if (lookingFor.numberOfParameters == inputTable.Length - 1 && inputTable [inputTable.Length - 1] != "")
-						lookingFor.realizeCommand (inputTable);
-					else
-						View.WriteMessage(string.Format ("error, komenda przyjmuje {0} argument(ow)", lookingFor.numberOfParameters));
+			switch (inputTable [0]) 
+			{
+				case "cd": Cd(inputTable); break;
+				case "ChState": ChState(inputTable); break;
+				case "ChName": ChName(inputTable); break;
+				case "AddNode": AddNode(inputTable); break;
+				case "DelNode": DelNode(inputTable); break;
+				case "Help": Help(); break;
+				default: break;
+				}
+		}
+		
+		public void Cd(params string[] args)
+		{
+			if (args [1] == "..") 
+			{
+				if(prevSubNodesList.Count<1)
+				{
+					currentLvl = 0;
+					currentNode = null;
+					prevSubNodesList.Clear();
+					subNodesList = mod.GetSubNodesList(0);
+				}
+				else
+				{
+					currentLvl--;
+					int lastNode = prevSubNodesList.Count-1;
+					currentNode = prevSubNodesList[lastNode];
+					prevSubNodesList = mod.GetPrevNodesList(currentNode.GetID());
+					subNodesList = mod.GetSubNodesList(currentNode.GetID());
+				}
+			}
+			else
+			{
+				if(subNodesList.Count==0) return;
+				foreach(Node temp in subNodesList)
+				{			
+
+					if(temp.name == args[1])
+					{
+						currentNode = temp;
+						currentLvl++;
+						subNodesList=mod.GetSubNodesList(temp.GetID());
+						prevSubNodesList = mod.GetPrevNodesList(temp.GetID());
+						break;
+					}
+				}
+			}
+
+
+		}
+		public void ChName(params string[] args)
+		{
+			string newName = args [2];
+			foreach (Node temp in subNodesList) 
+			{
+				if(temp.name == args[1])
+				{
+					temp.name = newName;
+					mod.Save(temp);
+					currentNode = temp;
 					return;
 				}
 			}
-			View.WriteMessage("nie ma takiej komendy");
+			View.WriteMessage("nie ma takiego podwezla");
+
+
+		}
+		public void ChState(params string[] args)
+		{
+			foreach (Node temp in subNodesList) 
+			{
+				if(temp.name == args[1])
+				{
+					switch(args[2])
+					{
+						case "completed" : temp.state= StateOfNode.completed; break;
+						case "uncompleted" : temp.state = StateOfNode.uncompleted; break;
+						default: View.WriteMessage("błędny stan!"); break;
+					}
+					mod.Save(temp);
+					currentNode = temp;
+					return;
+				}
+			}
+			View.WriteMessage("nie ma takiego podwęzła!");
+		}
+
+		public void AddNode(params string[] args)
+		{
+			UInt32 newID = 1000; //tymczasowa wartosc
+			Node newNode = new Node(args[1],StateOfNode.uncompleted,newID,currentLvl+1);
+			if(currentLvl!=0)
+				mod.NewNode(currentNode.GetID(),newNode);
+			else mod.NewNode(true,newNode);
+			subNodesList.Add(newNode);
+
+		}
+
+		public void DelNode(params string[] args)
+		{
+			foreach (Node tmp in subNodesList) 
+			{
+				if(tmp.name == args[1])
+				{
+					DestroyChildren(tmp);
+					if(currentLvl==0)
+						subNodesList = mod.GetSubNodesList(0);
+					else
+						subNodesList = mod.GetSubNodesList(currentNode.GetID());
+				}
+			}
+		}
+		void DestroyChildren (Node current)
+		{
+			List<Node> children = mod.GetSubNodesList (current.GetID ());
+			if (children.Count == 0)
+				mod.Delete (current.GetID());
+			else 
+			{
+				foreach(Node tmp in children)
+				{
+					DestroyChildren(tmp);
+					children = mod.GetSubNodesList(current.GetID());
+				}
+				DestroyChildren(current);
+			}
+		}
+
+
+		public void Help()
+		{
 
 		}
 	}
+
 }
 
--- planerConsole_1/planerConsole_1/Program.cs
+++ planerConsole_1/planerConsole_1/Program.cs
@@ -1,37 +1,27 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
+
 namespace planerConsole_1
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
+			Console.WriteLine ("Hello World!");
+
 			//TESTOWANIE TUTAJ ;)
-			UInt32 cos = 1;
-			Model mod = new Model(@"/home/ramzes/Dokumenty/mono-workspace/planer2/nowytyp.txt");
-			List<Node> lista = mod.GetSubNodesList(cos);
+			Model mod = new Model ("/home/kamil/Desktop/repositories/connector/nowytyp.txt");
+			Controller conductor = new Controller (mod);
+			while (true) 
+			{
+				Console.Write(":>> ");
+				conductor.controllerInput(Console.ReadLine());
 
-			Node nowyW;
-			nowyW = new Node("NOWY WEZEL1");
-			mod.NewNode(1,nowyW);
 
-			nowyW = new Node("NOWY WEZEL2");
-			mod.NewNode(1,nowyW);
-
-			nowyW = new Node("NOWY WEZEL3");
-			mod.NewNode(1,nowyW);
-
-			nowyW = mod.GetNode(11);
-			nowyW.name = "ZMIENIONA NAZWA1";
-			mod.Save(nowyW);
-
-			mod.Delete(12);
-
-			lista = mod.GetSubNodesList(cos);
-			mod.Close();
 			//TESTOWANIE TUTAJ ;)
-			Console.ReadLine();
+
+			//Console.ReadLine();
+			}
 		}
 	}
 }
